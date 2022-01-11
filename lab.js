5b961875af0f8c09519e9649724e40d601a9f8b4
Wesley Lo <wesleylo1995@gmail.com>
2022-01-11 15:57:58 -0600

    lab complete

--- lab.js
+++ lab.js
@@ -381,14 +381,24 @@ console.log(helensInfo);
 */
 
 //Code Here 
-
+class Vehicle {
+  constructor(capacity, color, mileage) {
+    this.capacity = capacity;
+    this.color = color;
+    this.mileage = mileage;
+  }
+  move(miles) {
+    this.mileage += miles
+    console.log(this.mileage);
+  }
+}
 
 /*
   Create a vehicle using your new class and save it to a variable called myFirstVehicle
 */
 
 //Code Here
-
+let myFirstVehicle = new Vehicle(4, 'red', 20,000)
 
 /* 
   Now we'll create a class that's based off of the vehicle class. 
@@ -399,16 +409,27 @@ console.log(helensInfo);
 */
 
 //Code Here
+class Motorcycle extends Vehicle {
+  constructor(capacity, color, mileage, make, isCool) {
+    super(capacity, color, mileage)
 
+    this.make = make;
+    this.isCool = isCool;
+  }
+}
 /*
   Create a Motorcycle using your new class and save it to a variable called myFirstMotorcycle
 */
 
 //Code Here 
+let myFirstMotorcycle = new Motorcycle(1,'black',5,'Yamaha',true)
 
 /*
   Call the move function on myFirstMotorcycle (don't forget the parameter)
 */
+myFirstMotorcycle.move(500)
+console.log(myFirstMotorcycle)
+
 
 /*
   Let's make another class based off of Vehicle. 
@@ -427,7 +448,27 @@ console.log(helensInfo);
 */
 
 //Code Here
+class Boat extends Vehicle {
+  constructor(capacity, color, mileage, name, type, isSeaworthy) {
+    super(capacity, color, mileage) 
 
+    this.name = name;
+    this.type = type;
+    this.isSeaworthy = isSeaworthy;
+  }
+checkSeaworthiness() {
+  if (this.isSeaworthy == true) {
+    console.log(`The ${this.color} ${this.type} ${this.name} is seaworthy!`)
+  } else {
+      console.log(`You need to get your ${this.type} in shape!`)
+  }
+}
+
+performMaintenance() {
+  this.isSeaworthy = true
+}
+
+}
 
 /*
   Create a new boat using your class. You can choose whatever values you like for all the 
@@ -435,21 +476,25 @@ console.log(helensInfo);
 */
 
 //Code Here
-
+let myFirstBoat = new Boat(4,'white', 1000,'Slice of Life','yacht', false)
 /*
   Call the checkSeaworthiness method on your new boat
 */
 
 //Code Here
-
+myFirstBoat.checkSeaworthiness()
+console.log(myFirstBoat);
 /*
   Now run the performMaintenance method on your boat
 */
 
 //Code Here 
-
+myFirstBoat.performMaintenance()
+console.log(myFirstBoat)
 /*
   Check the seaworthiness once more (you should be ready for the water!)
 */
 
 //Code Here
+myFirstBoat.checkSeaworthiness()
+console.log(myFirstBoat);
